a611e45861e6ca5b13eaa540534475ddf7076c76
Yazeed Alkalmi <115902643+Zeedart@users.noreply.github.com>
2025-09-21 20:11:00 +0200

    Added Navbar

---
+++ Smart-Study-Dashboard/src/components/Navbar.jsx
@@ -0,0 +1,23 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import pfp from "../assets/PFP.jpg"
+import styles from "../components/styles/navBar.module.css"
+
+/* import all the icons in Free Solid, Free Regular, and Brands styles */
+import { faLanguage, faMoon } from '@fortawesome/free-solid-svg-icons'
+
+
+export default function Navbar() {
+  return (
+    <div className={`${styles.container}`}>
+      <div className={`${styles.profile}`}>
+        <img src={pfp} width={50} height={50} style={{ borderRadius: 100 }} />
+        <p>NAME</p>
+      </div>
+      <div className={`${styles.settings}`}>
+        <FontAwesomeIcon className={`${styles.lang}`} icon={faLanguage}/>
+
+        <FontAwesomeIcon className={`${styles.theme}`} icon={faMoon}/>
+      </div>
+    </div>
+  )
+}
